c7ef2b328238a75dc82462a3df481074a59bdbd0
Uziel Linares <uziel.linares@protonmail.com>
2023-07-23 14:29:21 -0600

    add new file remap

--- lua/devils-canyon/remap.lua
+++ lua/devils-canyon/remap.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+vim.keymap.set("n", "<leader>nf", function()
+    local new_file = vim.fn.input("Name of new file > ")
+    vim.cmd { cmd = "vs", args = { "%:h/" .. new_file } }
+end)
